c7db8e427aa275d0e0b2a4934c91eb1d042c19f7
JoaoVitorLima242 <jlima0500@gmail.com>
2024-01-09 20:44:07 -0300

    feat: create tags migration

--- src/database/ormCliConfig.ts
+++ src/database/ormCliConfig.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCourseTable1704841911699 } from 'src/migrations/1704841911699-CreateCourseTable';
+import { CreateTagsTable1704843280081 } from 'src/migrations/1704843280081-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCourseTable1704841911699],
+  migrations: [CreateCourseTable1704841911699, CreateTagsTable1704843280081],
 });
