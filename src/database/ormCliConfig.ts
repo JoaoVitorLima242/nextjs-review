460321652f7bf2d8b52ce66897e9b0f018269f54
C5363386 <joao.vitor.brietzke.de.lima@sap.com>
2024-01-11 12:19:10 -0300

    feat: creating relation table

--- src/database/ormCliConfig.ts
+++ src/database/ormCliConfig.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCourseTable1704841911699 } from 'src/migrations/1704841911699-CreateCourseTable';
 import { CreateTagsTable1704843280081 } from 'src/migrations/1704843280081-CreateTagsTable';
 import { AddTimestampCollumnTag1704843443853 } from 'src/migrations/1704843443853-AddTimestampCollumnTag';
+import { CreateCoursesTagsRelationshipTable1704985697543 } from 'src/migrations/1704985697543-CreateCoursesTagsRelationshipTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCourseTable1704841911699,
     CreateTagsTable1704843280081,
     AddTimestampCollumnTag1704843443853,
+    CreateCoursesTagsRelationshipTable1704985697543,
   ],
 });
---
+++ src/migrations/1704985697543-CreateCoursesTagsRelationshipTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCoursesTagsRelationshipTable1704985697543
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'courses_tags',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('courses_tags');
+  }
+}
